9c2353c4d722f08ca260dbc2a4a3156271c08b53
flavioaluis <flavio.aluis@gmail.com>
2023-08-10 15:14:46 -0300

    Add api to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import AdmRestaurantes from './paginas/Administracao/Restaurantes';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route path="/admin/restaurantes" element={<AdmRestaurantes />} />
     </Routes>
   );
 }
